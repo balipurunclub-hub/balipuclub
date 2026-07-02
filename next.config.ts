aef63a5b74b743ce0c397af4d1ccf1f40524d04a
srijankulal <srijankulal1010@gmail.com>
2026-07-02 09:34:13 +0530

    chore: add jwks-rsa and jose to serverExternalPackages in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['firebase-admin'],
+  serverExternalPackages: ['firebase-admin', 'jwks-rsa', 'jose'],
   allowedDevOrigins: ['1a0b-103-178-182-178.ngrok-free.app'],
 };
 
