1b40c1fdd95ea98679e2aaeeb43540c802edfc2d
srijankulal <srijankulal1010@gmail.com>
2026-07-02 09:53:20 +0530

    chore: override jwks-rsa version and update allowed dev origin in next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['firebase-admin', 'jwks-rsa', 'jose'],
-  allowedDevOrigins: ['1a0b-103-178-182-178.ngrok-free.app'],
+  allowedDevOrigins: ['balipu.vercel.app'],
 };
 
 export default nextConfig;
